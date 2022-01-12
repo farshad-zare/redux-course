cdaf8e1ed26c7d4a8495d33bb93aeb3e3c6ba38d
Farshad Zare <farshad.z@outlook.com>
2022-01-12 23:25:12 +0330

    vid7 completed

---
+++ vid7/index.js
@@ -0,0 +1,70 @@
+const actions = [
+  {
+    type: "ADD_TODO",
+    todo: {
+      id: 0,
+      name: "Learn Redux",
+      complete: false,
+    },
+  },
+
+  {
+    type: "REMOVE_TODO",
+    id: 0,
+  },
+
+  {
+    type: "TOGGLE_TODO",
+    id: 0,
+  },
+
+  {
+    type: "ADD_GOAL",
+    goal: {
+      id: 0,
+      name: "Run a Marathon",
+    },
+  },
+
+  {
+    type: "REMOVE_GOAL",
+    id: 0,
+  },
+];
+// The actions was created beacause of prettier
+
+function todos(state = [], action) {
+  if (action.type === "ADD_TODO") {
+    return state.concat([action.todo]);
+  }
+
+  return state;
+}
+
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((l) => {
+        l !== listener;
+      });
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
