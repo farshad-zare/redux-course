104f1379ca73660223148aa6ee9a90025179889e
Farshad Zare <farshad.z@outlook.com>
2022-01-29 22:21:17 +0330

    vid20 completed

---
+++ vid20/store.js
@@ -0,0 +1,28 @@
+//store code
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((l) => {
+        l !== listener;
+      });
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
